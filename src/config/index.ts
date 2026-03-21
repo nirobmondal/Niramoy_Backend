5ff1ca7f065d6e48d9338810241380ef02c1f35f
nirobmondal <nirobmondal202@gmail.com>
2026-03-21 11:18:57 +0600

    env updated

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 export const config = {
   port: process.env.PORT || 5000,
   databaseUrl: process.env.DATABASE_URL!,
-  frontendUrl: process.env.FRONTEND_URL || "http://localhost:5000",
+  frontendUrl: process.env.FRONTEND_URL || "http://localhost:3000",
   betterAuthUrl: process.env.BETTER_AUTH_URL!,
   google: {
     clientId: process.env.GOOGLE_CLIENT_ID!,
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,50 +4,43 @@ import { prisma } from "./prisma";
 import { userRole } from "../constant/role";
 import { config } from "../config";
 
-
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", 
-    }),
-    trustedOrigins: [config.frontendUrl],
-    advanced: {
-        defaultCookieAttributes: {
-            sameSite: "none",
-            secure: process.env.NODE_ENV === "production",
-        }
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  trustedOrigins: [config.frontendUrl],
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: userRole.CUSTOMER,
+        required: false,
+      },
+      phone: {
+        type: "string",
+        required: false,
+      },
+      address: {
+        type: "string",
+        required: false,
+      },
+      isBanned: {
+        type: "boolean",
+        defaultValue: false,
+      },
     },
-    user: {
-        additionalFields: {
-            role: {
-                type: "string",
-                defaultValue: userRole.CUSTOMER,
-                required: false
-            },
-            phone: {
-                type: "string",
-                required: false
-            },
-            address: {
-                type: "string",
-                required: false
-            },
-            isBanned: {
-                type: "boolean",
-                defaultValue: false
-            }
-        }
+  },
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  baseURL: config.betterAuthUrl,
+  socialProviders: {
+    google: {
+      clientId: config.google.clientId,
+      clientSecret: config.google.clientSecret,
+      accessType: "offline",
+      prompt: "select_account consent",
     },
-    emailAndPassword: { 
-        enabled: true, 
-        autoSignIn: false
-    },
-    baseURL: config.betterAuthUrl, 
-    socialProviders: {
-        google: { 
-            clientId: config.google.clientId, 
-            clientSecret: config.google.clientSecret, 
-            accessType: "offline", 
-            prompt: "select_account consent",
-        }, 
-    },
+  },
 });
